a67f64e3224622638c9a72623549aba754d4c55a
Aobadjke5 <zyy152roy@163.com>
2024-06-12 21:06:12 +0800

    升级实现用户管理列表的向下滚动加载2

--- src/main/java/com/zyy/dao/UserMapper.java
+++ src/main/java/com/zyy/dao/UserMapper.java
@@ -28,7 +28,7 @@ public interface UserMapper {
             "CompanyName as companyName, CompanyIcon as CompanyIcon, CompanyAddress as companyAddress, " +
             "PeopleName as peopleName, PeopleTel as peopleTel, PeopleMail as PeopleMail " +
             "from T_User where Status = 'Verified' " +
-            "AND UserName LIKE CONCAT('%', #{kw}, '%') LIMIT #{size} OFFSET #{start}")
+            "AND CompanyName LIKE CONCAT('%', #{kw}, '%') LIMIT #{size} OFFSET #{start}")
     ArrayList<User> getUserList(@Param("start") Integer start, @Param("size") Integer pageSize, @Param("kw") String keyWord);
 
     @Select("SELECT UserID as userID, UserName as userName, Role as role, Status as Status, " +
